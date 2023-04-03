02e3c68790f8f723d2f3ad527593b7bcfad86524
Deborah Wyny <deborahwyny25@gmail.com>
2023-04-02 21:43:27 -0300

    final commit

--- index.js
+++ index.js
@@ -1,30 +1,25 @@
 function selecionar(seletor) {
   const btn = document.querySelector(".principais .green");
-  const icon = document.querySelector('.green .icon')
-
+  const icon = document.querySelector('.principais .green .icon')
   
   const selecionado = document.querySelector(seletor);
   selecionado.classList.add("green");
-  
-  
-
   
   if (btn !== null) {
     btn.classList.remove("green");
     icon.classList.add('escondido')
   }
 
-
   const iconSelecionado = document.querySelector('.green .icon')
   iconSelecionado.classList.remove('escondido')
 
-  
-  
+  verificarBtn()
 }
 
 function selecionarDrinks(seletor) {
   const btn2 = document.querySelector('.bebidas .green')
-   const icon = document.querySelector('.green .icon')
+  const icon = document.querySelector('.bebidas .green .icon')
+
   if (btn2 !== null) {
     btn2.classList.remove("green");
     icon.classList.add('escondido')
@@ -34,15 +29,16 @@ function selecionarDrinks(seletor) {
   const selecionado2 = document.querySelector(seletor);
   selecionado2.classList.add("green");
 
-  const iconSelecionado = document.querySelector('.green .icon')
+  const iconSelecionado = document.querySelector('.bebidas .green .icon')
   iconSelecionado.classList.remove('escondido')
   
+  verificarBtn()
   
 }
 
 function selecionarDesserts(seletor) {
   const btn3 = document.querySelector(".sobremesas .green");
-  const icon = document.querySelector('.green .icon')
+  const icon = document.querySelector('.sobremesas .green .icon')
 
   if (btn3 !== null) {
     btn3.classList.remove("green");
@@ -53,44 +49,42 @@ function selecionarDesserts(seletor) {
   const selecionado3 = document.querySelector(seletor);
   selecionado3.classList.add("green");
 
-  const iconSelecionado = document.querySelector('.green .icon')
+  const iconSelecionado = document.querySelector('.sobremesas .green .icon')
   iconSelecionado.classList.remove('escondido')
   
   
   
-  
+  verificarBtn()
 }
 
 
-// function aplicadorIcon (seletor) {
-  
-  
-//   const icon = documento.querySelector('.green')
-//   if (btn.classList.contains('green')) {
-//     icon.classList.add('icon')
-//   }
-  
-// }
+function verificarBtn () {
+const selecionados = document.querySelectorAll('.green');
+if (selecionados.length === 3) {
+  const btn = document.querySelector('.btn');
+  btn.classList.add('disabled');
+  const btnVerde = document.querySelector('.btn-verde');
+  btnVerde.classList.remove('disabled');
+}
 
 
+}
 
+function fecharPedido () {
+let prato = document.querySelector('.principais .green .nome').innerHTML;
+let bebida = document.querySelector('.bebidas .green .nome').innerHTML;
+let sobremesas = document.querySelector('.sobremesas .green .nome').innerHTML;
 
+let price = document.querySelector('.principais .green .priceValue');
+let price2 = document.querySelector('.bebidas .green .priceValue');
+let price3 = document.querySelector('.sobremesas .green .priceValue');
 
+let valorFinal = parseFloat(price.innerHTML) + parseFloat(price2.innerHTML) + parseFloat(price3.innerHTML);
 
-// function aplicadorIcon(seletor) {
-//   const icon = document.querySelector('.principais .icon')
-//   if (icon !== null) {
-//     icon.classList.remove('escondido')
-//     icon.classList.add('icon')
-//   }
-//   else {
-//     icon.classList.add('escondido')
-//     icon.classList.remove('icon')
-//   }
+let textoTop = `Olá, gostaria de fazer o pedido:\n - Prato: ${prato}\n - Bebida: ${bebida}\n - Sobremesa: ${sobremesas}\nTotal: ${valorFinal.toFixed(2)}`
 
-//   const selecionado4 = document.querySelector(seletor);
-//   selecionado4.classList.add('icon')
-
-// }
+let textoEncodade = encodeURIComponent(textoTop)
+  window.location.assign('https://wa.me/5524999037329?text='+ textoEncodade) 
+}
 
 
